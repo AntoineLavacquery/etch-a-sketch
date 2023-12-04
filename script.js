a0050c1e49b2525fff34b27b425ddf0b9bb17eb8
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2023-12-04 23:47:46 +0100

    Add correct mouse click-holding for drawing

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+let mouseDown = false;
+window.addEventListener("mousedown", () => {mouseDown = true;})
+window.addEventListener("mouseup", () => {mouseDown = false;})
 
 let grid = document.querySelector(".grid");
-
 const GRID_SIZE = 16;
 
 for (let i = 0; i < GRID_SIZE; i++) {
@@ -8,8 +10,15 @@ for (let i = 0; i < GRID_SIZE; i++) {
     line.className = "line";
     for (let j = 0; j < GRID_SIZE; j++) {
         let cell = document.createElement("div");
-        cell.className = "cell"
-        cell.innerText = `${i}${j}`;
+        cell.className = "cell";
+        cell.addEventListener("mouseenter", function() {
+            if (mouseDown) {
+                this.style.backgroundColor = "red";
+            }
+        })
+        cell.addEventListener("mousedown", function() {
+            this.style.backgroundColor = "black";
+        })
         line.appendChild(cell);
     }
     grid.appendChild(line);
