ce8bd92fe78dc61548917c6a1f0c84b97cb7351b
Antoine LAVACQUERY <antoine.lavacquery@proton.me>
2023-12-04 22:51:55 +0100

    Add line repartition

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ let grid = document.querySelector(".grid");
 const GRID_SIZE = 16;
 
 for (let i = 0; i < GRID_SIZE; i++) {
+    let line = document.createElement("div");
+    line.className = "line";
     for (let j = 0; j < GRID_SIZE; j++) {
         let cell = document.createElement("div");
+        cell.className = "cell"
         cell.innerText = `${i}${j}`;
-        grid.appendChild(cell);
+        line.appendChild(cell);
     }
+    grid.appendChild(line);
 }
